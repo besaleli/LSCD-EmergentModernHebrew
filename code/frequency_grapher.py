96f58b78ce28099b82bc3b260b945453bcad22b9
Raz Besaleli <rbesaleli@gmail.com>
2022-01-07 14:12:04 -0500

    Update frequency_grapher.py

--- code/frequency_grapher.py
+++ code/frequency_grapher.py
@@ -12,26 +12,30 @@ def getWUMs(tok):
     print('getting wums...')
     for name in tqdm(glob.glob('byp_decade_wums/*.pickle')):
         yr = int(name[16:20])
+        yrs.append(yr)
         if 1880 <= yr <= 1950:
             with open(name, 'rb') as f:
                 w = pickle.load(f)
                 f.close()
 
+            year_length = 0
+
             if tok in w.keys():
-                yrs.append(yr)
-                year_length = 0
                 for t, u in w.items():
                     year_length += len(u)
                     if t == tok:
                         wums.append(u)  # get WUMs
 
-                master_length.append(year_length)   # get total number of tokens in year
+            else:
+                wums.append(None)
+
+            master_length.append(year_length)   # get total number of tokens in year
 
     return yrs, wums, master_length
 
 
 def graph_diachronic_usage(wums, years, norms, tok):
-    freqs = [len(w) / norm for w, norm in zip(wums, norms)]
+    freqs = [0 if w is None else len(w) / norm for w, norm in zip(wums, norms)]
 
     plt.bar(years, freqs, color='black', width=1)
 
